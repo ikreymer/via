f7e70b06f2a02eb62588648543a3f124053ed7f1
Ilya Kreymer <ikreymer@gmail.com>
2015-04-17 12:48:20 -0700

    add redirect handler from /h/ -> / update to use pywb 0.9.5!

--- pywb_h.py
+++ pywb_h.py
@@ -1,6 +1,17 @@
 from pywb.webapp.live_rewrite_handler import RewriteHandler
 from pywb.framework.wbrequestresponse import WbResponse
 
+from pywb.framework.basehandlers import BaseHandler
+
+
+class SimpleRedirHandler(BaseHandler):
+    def __call__(self, wbrequest):
+        if wbrequest.wb_url_str == '/':
+            wbrequest.wb_url_str = ''
+        return WbResponse.redir_response(self.redir_path + wbrequest.wb_url_str,
+                                         self.status)
+
+
 class CustomRedirHandler(RewriteHandler):
     def __init__(self, config):
         super(CustomRedirHandler, self).__init__(config)

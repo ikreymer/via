6759827633e9a3f6941081e88bcab1ec8a54c6f0
Nick Stenning <nick@whiteink.com>
2015-09-25 18:38:17 +0200

    Serve a redirect for the old PDF viewer
    This commit adds a redirect for the old PDF viewer, redirecting paths
    such as
    
        /static/__shared/viewer/web/viewer.html?file=http://example.com/test.pdf
    
    to the new path,
    
        /http://example.com/test.pdf
    
    In order to do this, we remove responsibility for serving static files
    and redirects from pywb, and wrap the application in a small werkzeug
    WSGI app.

--- via/rewriter.py
+++ via/rewriter.py
@@ -1,21 +1,11 @@
-import urllib
-
 import jinja2
 
 from pywb.webapp.live_rewrite_handler import RewriteHandler
-from pywb.framework.basehandlers import BaseHandler
 from pywb.framework.wbrequestresponse import WbResponse
+
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates/',
                                                         encoding='utf-8-sig'))
-
-
-class SimpleRedirHandler(BaseHandler):
-    def __call__(self, wbrequest):
-        if wbrequest.wb_url_str == '/':
-            wbrequest.wb_url_str = ''
-        return WbResponse.redir_response(self.redir_path + wbrequest.wb_url_str,
-                                         self.status)
 
 
 class TemplateRewriteHandler(RewriteHandler):
